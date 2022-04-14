7397895e24722acddb24a6c3368df3b20b0407fc
Balaga Indira <balagasateeshspis@gmail.com>
2022-04-14 14:16:40 +0530

    Added Login Forms

--- src/app/virtual-desk/components/faculty-login/faculty-login.component.ts
+++ src/app/virtual-desk/components/faculty-login/faculty-login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { FormControl, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-faculty-login',
@@ -7,9 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FacultyLoginComponent implements OnInit {
 
-  constructor() { }
+  isFormSubmitted: boolean;
+  facultyForm: FormGroup;
+  validationMessages = {
+      facultyId: [
+          { type: 'required', message: 'Faculty ID is required.' },
+          { type: 'pattern', message: 'Faculty ID is of 10 digits.' }
+        ],
+      password: [
+          { type: 'required', message: 'Password is required.'}
+      ]
+    };
+  validationPattern = {
+    facultyId: new RegExp(`[0-9]{10}$`),
+  };
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private firestore: AngularFirestore,
+    private toastr: ToastrService
+  ) {
+      this.isFormSubmitted = false;
+
+  }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.facultyForm = this.formBuilder.group({
+      facultyId: new FormControl(
+        '', [Validators.required, Validators.pattern(this.validationPattern.facultyId), Validators.maxLength(10)]
+      ),
+      password: new FormControl(
+        '', [Validators.required]
+      )
+    })
+  }
+
+  submitFacultyForm(): void {
+    
+  }
+
+  resetForm(): void {
+    this.facultyForm.reset();
   }
 
 }
--- src/app/virtual-desk/components/student-login/student-login.component.ts
+++ src/app/virtual-desk/components/student-login/student-login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { FormControl, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-student-login',
@@ -7,9 +11,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentLoginComponent implements OnInit {
 
-  constructor() { }
+  isFormSubmitted: boolean;
+  studentForm: FormGroup;
+  validationMessages = {
+      rollNumber: [
+          { type: 'required', message: 'Roll Number is required.' },
+          { type: 'pattern', message: 'Roll Number is of 10 digits.' }
+        ],
+      password: [
+          { type: 'required', message: 'Password is required.'}
+      ]
+    };
+  validationPattern = {
+    rollNumber: new RegExp(`[0-9]{10}$`),
+  };
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private firestore: AngularFirestore,
+    private toastr: ToastrService
+  ) {
+      this.isFormSubmitted = false;
+
+  }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.studentForm = this.formBuilder.group({
+      rollNumber: new FormControl(
+        '', [Validators.required, Validators.pattern(this.validationPattern.rollNumber), Validators.maxLength(10)]
+      ),
+      password: new FormControl(
+        '', [Validators.required]
+      )
+    })
+  }
+
+  submitStudentForm(): void {
+    this.isFormSubmitted = true;
+  }
+
+  resetForm(): void {
+    this.studentForm.reset();
   }
 
 }
